4b03dc6335376d3ed79f5eb45a36c2c78dd672fa
lakshmanacentria <laxam@acentriasoftware.com>
2020-02-27 15:27:16 +0530

    added space counter in string

--- app/src/main/java/com/techgen/multipleviewpager/MainActivity.kt
+++ app/src/main/java/com/techgen/multipleviewpager/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.techgen.multipleviewpager
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     private var adapter: SampleAdapter? = null
     private var tabs: TabLayout? = null
     private var pager: ViewPager? = null
     private val category = arrayOf("a", "b", "50", "d", "e", "f", "g")
+    var name:String="I am tejus gupta"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,6 +31,16 @@ class MainActivity : AppCompatActivity() {
 
         adapter!!.setPageCount(category.size)
         adapter!!.notifyDataSetChanged()
+
+
+        //to check space in String
+        val pattern = Pattern.compile(" ")
+        val matcher = pattern.matcher(name)
+        var count = 0
+        while (matcher.find()) {
+            count++
+        }
+        Log.d("spaceChane"," "+count)
     }
 
 }
